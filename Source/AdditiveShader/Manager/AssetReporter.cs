5b0a7238587359140d77e3b86c67f66b222e9bc3
aubergine10 <guy.fraser1@gmail.com>
2020-06-03 04:15:19 +0100

    Update AssetReporter - Tabulate report and fix stopwatch
    Report is now tabular - much easier to read.
    
    Note: Still trying to find better alternative to `PadRight()`.
    
    Also made the timer public as it needs to be started quite a bit before the reporter instance is made.

--- Source/AdditiveShader/Manager/AssetReporter.cs
+++ Source/AdditiveShader/Manager/AssetReporter.cs
@@ -1,6 +1,7 @@
 namespace AdditiveShader.Manager
 {
     using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
     using System.Text;
 
     /// <summary>
@@ -11,21 +12,26 @@ namespace AdditiveShader.Manager
     {
         private readonly StringBuilder report;
 
-        private readonly Stopwatch timer;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="AssetReporter"/> class.
         /// </summary>
         internal AssetReporter()
         {
-            timer = Stopwatch.StartNew();
-
             report = new StringBuilder(2048);
 
             report
                 .AppendLine("[AdditiveShader] Assets using shader:")
+                .AppendLine()
+                .AppendLine("Static AlwaysOn Remote OverMid DayOnly NightOnly Twilight Type            Name 'm_mesh.name'")
+                .Append('-', 100)
                 .AppendLine();
         }
+
+        /// <summary>
+        /// <para>Gets or sets the report timer.</para>
+        /// <para>The timer is initialised by <see cref="AdditiveShaderManager.Start()"/>.</para>
+        /// </summary>
+        public static Stopwatch Timer { get; set; }
 
         /// <summary>
         /// Adds details of an asset to the report.
@@ -33,17 +39,17 @@ namespace AdditiveShader.Manager
         /// <param name="shader">The shader-using asset to report on.</param>
         internal void Append(ShaderAsset shader) =>
             report
-                .Append(shader).AppendLine(":")
-                .Append("- ").Append(shader.Info)
-                .AppendLine()
-                .Append("- Static: ").Append(shader.Info.IsStatic)
-                .Append(", Always On: ").Append(shader.Info.IsAlwaysOn)
-                .Append(", Remote Control: ").Append(shader.Info.IsRemotelyControlled)
-                .Append(", Overlap Midnight: ").Append(shader.Info.OverlapsMidnight)
-                .Append(", Day Only: ").Append(shader.Info.IsDayTimeOnly)
-                .Append(", Night Only: ").Append(shader.Info.IsNightTimeOnly)
-                .Append(", Twilight: ").Append(shader.Info.IsToggledByTwilight)
-                .AppendLine()
+                .AppendFormat(
+                    "{0} {1} {2} {3} {4} {5} {6} {7} '{8}'",
+                    (shader.Info.IsStatic             ? "Yes" : "-").PadRight(6),
+                    (shader.Info.IsAlwaysOn           ? "Yes" : "-").PadRight(8),
+                    (shader.Info.IsRemotelyControlled ? "Yes" : "-").PadRight(6),
+                    (shader.Info.OverlapsMidnight     ? "Yes" : "-").PadRight(7),
+                    (shader.Info.IsDayTimeOnly        ? "Yes" : "-").PadRight(7),
+                    (shader.Info.IsNightTimeOnly      ? "Yes" : "-").PadRight(9),
+                    (shader.Info.IsToggledByTwilight  ? "Yes" : "-").PadRight(8),
+                    shader,
+                    shader.Info)
                 .AppendLine();
 
         /// <summary>
@@ -51,15 +57,15 @@ namespace AdditiveShader.Manager
         /// </summary>
         /// <param name="countAll">Total number of shader-usuing assets.</param>
         /// <param name="countTwilight">Number of twilight time-based shaders.</param>
-        /// <param name="countGeneral">Number of general time-based shaders.</param>
-        internal void Summary(int countAll, int countTwilight, int countGeneral) =>
+        /// <param name="countTimeBased">Number of general time-based shaders.</param>
+        internal void Summary(int countAll, int countTwilight, int countTimeBased) =>
             report
+                .AppendLine()
                 .Append("Scanned ").Append(AssetScanner.ItemsScanned).Append(" assets ")
-                .Append("in ").Append(timer.ElapsedMilliseconds).Append("ms: ")
-                .Append("Found ").Append(countAll).Append(" additive shaders")
-                .Append(" (static: ").Append(countAll - countTwilight - countGeneral)
-                .Append(", day/night: ").Append(countTwilight)
-                .Append(", others: ").Append(countGeneral).Append(" ) ");
+                .Append("in ").Append(Timer.ElapsedMilliseconds).Append("ms: ")
+                .Append("Found ").Append(countAll).Append(" additive shaders (")
+                .Append(countTwilight).Append(" twilight-toggled, ")
+                .Append(countTimeBased).Append(" other time-based)");
 
         /// <summary>
         /// Publish report to the game log file.
