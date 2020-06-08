1bc30f2a8396e40f72073f80a38b84270b9e4c9b
aubergine10 <guy.fraser1@gmail.com>
2020-06-08 02:05:11 +0100

    Refresh game RenderGroups when render distances are altered
    This should solve issues with updated render distances not taking immediate effect, for example after a game load, or after changing ULOD settings.

--- Source/AdditiveShader/Manager/AdditiveShaderManager.cs
+++ Source/AdditiveShader/Manager/AdditiveShaderManager.cs
@@ -31,7 +31,7 @@ namespace AdditiveShader.Manager
         /// <para>Lookup of remote group <see cref="Guid"/> to associated assets.</para>
         /// <para>A group is only added to the dictionary if it contains one or more assets.</para>
         /// </summary>
-        private Dictionary<Guid, IEnumerable<ManagedAsset>> remoteGroups;
+        private Dictionary<Guid, List<ManagedAsset>> remoteGroups;
 
         /// <summary>
         /// A list containing all shader-using assets.
@@ -138,34 +138,35 @@ namespace AdditiveShader.Manager
 
             if (managedAssets.Count == 0)
             {
-                Debug.Log("[AdditiveShader] No shader-using assets found. Manager is shutting down.");
                 OnDestroy();
                 return;
             }
 
-            PrepareAssetCategories(managedAssets.Count);
-            SortAssetsInToCategories(); // also generates report
-            CompactAssetCategories();
+            RefreshRenderGroups();
 
-            remoteGroups = new Dictionary<Guid, IEnumerable<ManagedAsset>>();
+            BuildCategories();
 
-            enabled = timeBasedAssets.Count != 0;
+            remoteGroups = new Dictionary<Guid, List<ManagedAsset>>();
+
+            instance = this;
 
             if (twilightAssets.Count != 0)
             {
-                InvokeRepeating(nameof(CheckForTwilight), TWILIGHT_CHECK_INTERVAL, TWILIGHT_CHECK_INTERVAL);
                 CheckForTwilight(FORCE);
+                InvokeRepeating(nameof(CheckForTwilight), TWILIGHT_CHECK_INTERVAL, TWILIGHT_CHECK_INTERVAL);
             }
-
-            instance = this;
+            else
+            {
+                enabled = timeBasedAssets.Count != 0;
+            }
 
             HookEvents(true);
         }
 
         /// <summary>
-        /// Checks to see if we have crossed twilight boundary.
+        /// If we've cross twilight (day/night) boundary, iterate the twilight assets.
         /// </summary>
-        /// <param name="force">On startup this is set true to ensure twilight buildings get set correctly.</param>
+        /// <param name="force">Only used by <c>Start()</c> to force a twilight iteration.</param>
         protected void CheckForTwilight(bool force = false)
         {
             bool isNightTimeNow = Singleton<SimulationManager>.instance.m_isNightTime;
@@ -206,16 +207,31 @@ namespace AdditiveShader.Manager
         /// <summary>
         /// Terminates the manager.
         /// </summary>
+        /// <param name="reason">The reason for shutdown (optional).</param>
         [UsedImplicitly]
         protected void OnDestroy()
         {
+            Debug.Log("[AdditiveShader] Manager shutting down.");
             HookEvents(false);
             instance = null;
             CancelInvoke();
             enabled = false;
             remoteGroups = null;
-            RestoreDefaults();
+            RestoreOriginalAssetSettings();
             DisposeAssetLists();
+        }
+
+        /// <summary>
+        /// After finishing an iteration, this works out what to do next.
+        /// </summary>
+        private void DetermineWhatHappensNext()
+        {
+            iterateTwilight = false;
+
+            count = timeBasedAssets.Count;
+            index = 0;
+
+            enabled = count > index;
         }
 
         /// <summary>
@@ -223,7 +239,7 @@ namespace AdditiveShader.Manager
         /// </summary>
         /// <param name="enable">Hooks when <c>true</c>, unhooks when <c>false</c>.</param>
         [SuppressMessage("Naming", "AV1738:Event handlers should be named according to the pattern '(InstanceName)On(EventName)'", Justification = "Overkill.")]
-        private static void HookEvents(bool enable)
+        private void HookEvents(bool enable)
         {
             var optionsPanel = UIView.library.Get<UIPanel>("OptionsPanel");
             var levelOfDetail = UIView.GetAView().FindUIComponent<UIDropDown>("LevelOfDetail");
@@ -241,51 +257,124 @@ namespace AdditiveShader.Manager
         }
 
         /// <summary>
-        /// Reapply render distance changes when options change, just in case.
+        /// Many things can overwrite render distances,
+        /// so reapply our own just in case.
         /// </summary>
-        /// <remarks>
-        /// This will often be superfluous, but the alternative is to start
-        /// integrating with stuff like ULOD and anything else that might
-        /// overwrite the render distances = PITA.
-        /// </remarks>
         /// <param name="component">Ignored.</param>
         /// <param name="visible">The visibility of the options panel.</param>
-        private static void OnOptionsPanelVisibilityChanged(UIComponent component, bool visible)
+        [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Game API")]
+        private void OnOptionsPanelVisibilityChanged(UIComponent component, bool visible)
         {
-            if (visible || instance is null)
-                return;
-
-            foreach (var asset in instance.managedAssets)
-                asset.ApplyCachedRenderDistance();
+            if (!visible)
+                instance.RefreshRenderDistances();
         }
 
         /// <summary>
-        /// Reapply render distance changes when level of detail is changed.
+        /// If 'Level Of Detail' game setting is changed,
+        /// it resets the render distances so we need to
+        /// reapply our own render distances.
         /// </summary>
         /// <param name="component">Ignored.</param>
         /// <param name="index">Ignored.</param>
+        [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Game API")]
         [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1625:Element documentation should not be copied and pasted")]
-        private static void OnLevelOfDetailChanged(UIComponent component, int index)
-        {
-            if (instance is null)
-                return;
+        private void OnLevelOfDetailChanged(UIComponent component, int index) =>
+            RefreshRenderDistances();
 
-            foreach (var asset in instance.managedAssets)
+        /// <summary>
+        /// In situations where render distances might be overwritten, we have to reapply them.
+        /// </summary>
+        private void RefreshRenderDistances()
+        {
+            foreach (var asset in managedAssets)
                 asset.ApplyCachedRenderDistance();
+
+            RefreshRenderGroups();
         }
 
         /// <summary>
-        /// After a complete iteration of either <see cref="timeBasedAssets"/>
-        /// or <see cref="twilightAssets"/>, work out what to do next.
+        /// After changing render distances, render groups need invalidating to push changes to renderer.
         /// </summary>
-        private void DetermineWhatHappensNext()
+        [SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Rarely invoked.")]
+        private void RefreshRenderGroups()
         {
-            iterateTwilight = false;
+            int buildingLayer = LayerMask.NameToLayer("Buildings");
+            int propsLayer    = LayerMask.NameToLayer("Props");
 
-            count = timeBasedAssets.Count;
-            index = 0;
+            foreach (var renderGroup in RenderManager.instance.m_groups)
+            {
+                if (renderGroup == null)
+                    continue;
 
-            enabled = count > index;
+                renderGroup.SetLayerDataDirty(buildingLayer);
+                renderGroup.SetLayerDataDirty(propsLayer);
+                renderGroup.UpdateMeshData();
+            }
+        }
+
+        /// <summary>
+        /// Time-based assets are sorted in to groups for easier management of update loop.
+        /// </summary>
+        private void BuildCategories()
+        {
+            PrepareAssetCategories(managedAssets.Count);
+            SortAssetsInToCategories(); // also generates report
+            CompactAssetCategories();
+        }
+
+        /// <summary>
+        /// Initialises category lists with plenty of capacity to minimise time-consuming
+        /// memory allocations, which in turn speeds up <see cref="SortAssetsInToCategories"/>.
+        /// </summary>
+        /// <param name="capacity">The initial capacity for the lists.</param>
+        private void PrepareAssetCategories(int capacity)
+        {
+            twilightAssets = new List<ManagedAsset>(capacity);
+            timeBasedAssets = new List<ManagedAsset>(capacity);
+        }
+
+        /// <summary>
+        /// There's no point iterating static assets, and there's no point iterating twilight
+        /// assets outside dawn/dusk, so filter in to context-specific lists to make iteration
+        /// more effective.
+        /// </summary>
+        /// <remarks>
+        /// Report could be done separtely, but doing it here saves an extra loop.
+        /// </remarks>
+        private void SortAssetsInToCategories()
+        {
+            var report = new AssetReporter();
+
+            foreach (var asset in managedAssets)
+            {
+                if (!asset.IsContainer && !asset.Info.IsStatic)
+                    (asset.Info.IsToggledByTwilight ? twilightAssets : timeBasedAssets)
+                        .Add(asset);
+
+                report.Append(asset);
+            }
+
+            report.Summary(managedAssets.Count, twilightAssets.Count, timeBasedAssets.Count);
+            report.PublishToLogFile();
+        }
+
+        /// <summary>
+        /// Lists were created with excess capacity; this trims that capacity to free RAM.
+        /// </summary>
+        private void CompactAssetCategories()
+        {
+            twilightAssets.TrimExcess();
+            timeBasedAssets.TrimExcess();
+        }
+
+        /// <summary>
+        /// Remove references to lists so GC can start reclaiming RAM sooner.
+        /// </summary>
+        private void DisposeAssetLists()
+        {
+            managedAssets = null;
+            twilightAssets = null;
+            timeBasedAssets = null;
         }
 
         /// <summary>
@@ -297,10 +386,10 @@ namespace AdditiveShader.Manager
         /// not already saved the altered values (ie. used earlier versions
         /// of the mod). ProTip: Always exit to desktop before using editor.
         /// </remarks>
-        private void RestoreDefaults()
+        private void RestoreOriginalAssetSettings()
         {
             foreach (var asset in managedAssets)
-                asset.RestoreDefaults();
+                asset.RestoreOriginalSettings();
         }
 
         /// <summary>
@@ -325,60 +414,6 @@ namespace AdditiveShader.Manager
                 Debug.Log($"[AdditiveShader] - {asset.TypeOfAsset} Refresh as AlwaysOn");
                 asset.SetVisible(true);
             }
-        }
-
-        /// <summary>
-        /// Initialises asset category lists:
-        /// <list type="bullet">
-        /// <item><see cref="twilightAssets"/> -- toggled at twilight (dusk and dawn)</item>
-        /// <item><see cref="timeBasedAssets"/> -- other time-based on/off</item>
-        /// </list>
-        /// </summary>
-        /// <param name="capacity">The initial capacity for the lists.</param>
-        private void PrepareAssetCategories(int capacity)
-        {
-            twilightAssets = new List<ManagedAsset>(capacity);
-            timeBasedAssets = new List<ManagedAsset>(capacity);
-        }
-
-        /// <summary>
-        /// <para>Sorts <see cref="managedAssets"/> in to categories and publishes report to log file.</para>
-        /// <para>Note: Report could be done separtely, but doing it here saves an extra loop.</para>
-        /// </summary>
-        private void SortAssetsInToCategories()
-        {
-            var report = new AssetReporter();
-
-            foreach (var asset in managedAssets)
-            {
-                if (!asset.IsContainer && !asset.Info.IsStatic)
-                    (asset.Info.IsToggledByTwilight ? twilightAssets : timeBasedAssets)
-                        .Add(asset);
-
-                report.Append(asset);
-            }
-
-            report.Summary(managedAssets.Count, twilightAssets.Count, timeBasedAssets.Count);
-            report.PublishToLogFile();
-        }
-
-        /// <summary>
-        /// Trims excess capacity from populated lists.
-        /// </summary>
-        private void CompactAssetCategories()
-        {
-            twilightAssets.TrimExcess();
-            timeBasedAssets.TrimExcess();
-        }
-
-        /// <summary>
-        /// Disposes the asset/category lists.
-        /// </summary>
-        private void DisposeAssetLists()
-        {
-            managedAssets = null;
-            twilightAssets = null;
-            timeBasedAssets = null;
         }
     }
 }
